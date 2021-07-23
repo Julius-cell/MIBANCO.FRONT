b3fb2f01adc35ff9b67f7c04f544de0262900ee1
Julio Cid <59150903+Julius-cell@users.noreply.github.com>
2021-07-23 11:39:39 -0400

    Add form template and everything needed

--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 
 import { ButtonModule } from 'primeng/button';
 import { TabMenuModule } from 'primeng/tabmenu';
-
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DropdownModule } from 'primeng/dropdown';
 
 @NgModule({
   exports: [
     ButtonModule,
     TabMenuModule,
+    InputTextModule,
+    InputNumberModule,
+    DropdownModule
   ]
 })
 export class PrimengModule { }
